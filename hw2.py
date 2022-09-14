3335d753b958b20b5fe4226d27b04768a3d46982
yauheniya <yauheniya@seznam.cz>
2022-09-14 22:02:59 +0300

    add hw2

---
+++ hw2.py
@@ -0,0 +1,50 @@
+# Exercise1
+# Data_list = ['temperature', 36.6, 38, [35, 41], 'pressure']
+# print(Data_list)
+# for i, el in enumerate(Data_list, 1):
+#     print(f'{i}, {el}')
+#     k = type(el)
+#     print(k)
+
+# Exercises2
+
+# Dl = ['temperature', 36.6, 38, [35, 41], 'pressure', 'blood', 'puls']
+# for i in range(1, len(Dl), 2):
+#     Dl[i - 1], Dl[i] = Dl[i], Dl[i - 1]
+#     print(Dl)
+
+
+# Exercise3
+#
+# month = {'winter': [1, 2, 12], 'spring': [3, 4, 5], 'summer': [6, 7, 8], 'autumn': [9, 10, 11]}
+# n = int(input('enter number:'))
+# for el in month:
+#     if n in month[el]:
+#         print(el)
+#     if n > 12:
+#         print('no month,no seasons')
+
+
+# Exercise4
+
+# slovo = input('enter world:').split(' ')
+# for i, el in enumerate(slovo, 1):
+#     if len(el) <= 10:
+#         print([i], el)
+#     else:
+#         print([i], el[0:10])
+
+# Exercise5
+# number = int(input('enter number:'))
+# my_list = [7, 5, 3, 3, 2]
+# i = 0
+# for el in my_list:
+#     if el >= number:
+#         i = i + 1
+# my_list.insert(i, number)
+# print(my_list)
+
+
+
+
+
